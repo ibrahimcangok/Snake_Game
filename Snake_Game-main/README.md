8ba7ed5e32bd29e978663ce7a773c420bf050ae9
ibrahim <ibrrahimgok00@gmail.com>
2024-01-26 12:11:58 +0300

    Initial Commit

---
+++ Snake_Game-main/README.md
@@ -0,0 +1,1 @@
+![snake_game_bitmap](https://github.com/ibrahimcangok/Snake-Game/assets/106431802/6e1ea696-77fb-479a-a547-dec66c79dbfc)
---
+++ Snake_Game-main/scoreboard.py
@@ -0,0 +1,44 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.high_score = self.read_high_score()
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def read_high_score(self):
+        try:
+            with open("data.txt", "r") as file:
+                return int(file.read())
+        except FileNotFoundError:
+            return 0
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
+
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("Game Over", align=ALIGNMENT, font=FONT)
+        self.reset()
